95a48e513bef378e04594ba4def26c9b552e9494
nirmit2007 <cnirmit07@gmail.com>
2026-05-29 18:11:25 +0530

    Time

--- Date/FileDate.py
+++ Date/FileDate.py
@@ -1,17 +1,12 @@
 import time
 
-# FUNCTION
 def show_time():
     print("Function called at :", time.ctime())
 
-
-# WRITE TIME IN FILE
 file = open("Date/log.txt", "a")
 
 file.write("Function called at : " + time.ctime() + "\n")
 
 file.close()
 
-
-# FUNCTION CALL
 show_time()
